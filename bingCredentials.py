cc1df1c8f93e96f73942c57475dfa6422b426f4d
Miguel Li <limingzhe792@gmail.com>
2016-08-07 09:42:29 -0400

    Create bingCredentials.py

---
+++ bingCredentials.py
@@ -0,0 +1,16 @@
+username1 = '...'
+password1 = '...'
+username2 = '...'
+password2 = '...'
+username3 = '...'
+password3 = '...'
+credentials = ()
+
+def getCredentials(num):
+	if num == 1:
+		return (username1, password1)
+	elif num == 2:
+		return(username2, password2)
+	elif num == 3:
+		return (username3, password3)
+
