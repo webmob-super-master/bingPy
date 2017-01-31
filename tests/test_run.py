177ed1db2f37edb20c30c6c9ae8884495cdb9dda
Miguel Li <limingzhe792@gmail.com>
2017-01-30 22:59:26 -0500

    initial commit

---
+++ tests/test_run.py
@@ -0,0 +1,11 @@
+from bingpy.py import manipulateBing
+
+def main():
+	for i in range(1,4):
+		chrome = manipulateBing()
+		chrome.login(i)
+		time.sleep(5)
+		chrome.search()
+		chrome.logout()
+
+main()
